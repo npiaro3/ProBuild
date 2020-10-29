5d6e3ba0c2dd900b1ed478951246e1ffee2d92d1
npiaro3 <npiaro3@gatech.edu>
2020-10-29 18:55:55 -0400

    Link Project to AWS Amplify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import {
+//   ApolloClient,
+//   ApolloProvider,
+//   HttpLink,
+//   InMemoryCache
+// } from "@apollo/client";
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link
+// })
 
 ReactDOM.render(<App />, document.getElementById('root'));
